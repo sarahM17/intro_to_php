0e3bbd1b8881bd37d3ce3345fe68f42dd4638a06
sarah <sarah.michaux@hotmail.be>
2019-05-16 14:53:29 +0200

    add string manipulation

---
+++ String manipulation/nickname.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String manipulation</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="row d-flex flex-column offset-md-4 justify-content-center ">
+        <h1>String manipulation</h1>
+    </div>
+    <div class="row d-flex flex-row justify-content-center">
+        <div class="col-md-5 flex-column justify-content-center row1">
+            <form name="Cool NickName Generator" method="post" action="";>
+                <h3>Cool nickname Generator</h3>
+                <input maxlength="30" name="item[0][nickName]" size="30" type="text" /><br>
+                <input type="submit" value="Submit" />
+            </form>
+            <div><?php
+
+                function show_nickname($e)
+                {
+                    return  "$e[nickName]";
+                }
+
+                $i = array_map("show_nickname", $_POST['item']);
+
+                echo '<ul>';
+                foreach($i as $item){
+                    echo '<li>'.$item.'</li>';
+                    echo '<li>'.strrev($item).'</li>';
+                    echo '<li>'.ucwords($item).'</li>';
+                    echo '<li>'.strrev($item).'</li>';
+                }
+
+                echo '</ul>'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
